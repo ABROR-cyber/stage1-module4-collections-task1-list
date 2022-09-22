bcab230c9eb58f4951ac1fc046c408ca9bc1ccf8
abror <abror01042001@gmail.com>
2022-09-22 20:57:29 +0500

    com

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,19 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int i = Math.abs(Integer.parseInt(a)) - Math.abs(Integer.parseInt(b));
+        return i==0&&a.startsWith("-")?-1:(i==0&&b.startsWith("-"))?1:i;
     }
 }
